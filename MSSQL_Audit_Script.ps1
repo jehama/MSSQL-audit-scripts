7e9c229c716d4255530c3ca3d3d8fdf38912838c
jehama <jehama@users.noreply.github.com>
2019-01-16 14:44:23 +0100

    Added comments for the Get-Help. Updated Parametersets.

--- MSSQL_Audit_Script.ps1
+++ MSSQL_Audit_Script.ps1
@@ -1,29 +1,79 @@
+<#
+.SYNOPSIS
+Audits the MSSQL Server against the CIS-benchmark, and looks at all users, roles and their rights.
+
+.DESCRIPTION
+This scripts checks the recommendations of the CIS-benchmark for MSSQL Server 2016 against the current configuration of the MSSQL Server.
+It will also display the following information from the database:
+    * The databases on the MSSQL Server.
+    * The logins and their corresponding database accounts.
+    * The roles that are defined.
+    * The rights granted or denied to users and roles.
+
+.PARAMETER Server
+Specifies the MSSQL Server to connect to.
+
+.PARAMETER Database
+Specifies the database to connect to.
+This parameter is optional. If no database is selected it will default to auditing all available databases.
+
+.PARAMETER WindowsAuthentication
+Specifies to use Windows Authentication when connecting to the MSSQL Server.
+
+.PARAMETER SQLAuthentication
+Specifies to use SQL Authentication when connecting to the MSSQL Server.
+
+.PARAMETER Username
+Specifies the username to use when authenticating to the MSSQL Server.
+This parameter is only used when authenticating with SQL Authentication.
+
+.INPUTS
+None.
+
+.OUTPUTS
+All output is printed on the console.
+If you wish to save the output add '> output.txt' after the command to run the script.
+This will save the output in a file called output.txt.
+
+.EXAMPLE
+.\MSSQL_Audit_Script.ps1 -Server "Servername" -WindowsAuthentication
+
+.EXAMPLE
+.\MSSQL_Audit_Script.ps1 -Server "Servername" -SQLAuthentication -Username "test"
+
+.EXAMPLE
+# When auditing a specific database:
+.\MSSQL_Audit_Script.ps1 -Server "Servername" -Database "DatabaseName" -WindowsAuthentication
+#>
+
 [CmdletBinding(DefaultParameterSetName="None")]
 # This initializes the parameters which were present when the script was launched.
 param(
-    # The MSSQL Server to connect to.
-    [parameter(Mandatory = $true)]
+    # Specifies the MSSQL Server to connect to.
+    [parameter(ParameterSetName = "WindowsAuthentication", Mandatory = $true)]
+    [parameter(ParameterSetName = "SQLAuthentication", Mandatory = $true)]
     [String]
     $Server,
 
-    # The database to connect to.
-    # This parameter is optional. Only use this if you wish to audit a specific database.
-    [parameter()]
+    # Specifies the database to connect to.
+    # This parameter is optional. If no database is selected it will default to auditing all available databases.
+    [parameter(ParameterSetName = "WindowsAuthentication")]
+    [parameter(ParameterSetName = "SQLAuthentication")]
     [String]
     $Database,
 
-    # Sets authentication form to Windows Authentication.
-    [parameter(ParameterSetName = "WindowsAuthentication")]
+    # Specifies to use Windows Authentication when connecting to the MSSQL Server.
+    [parameter(ParameterSetName = "WindowsAuthentication", Mandatory = $true)]
     [switch]
     $WindowsAuthentication,
 
-    # Sets authentication form to SQL Authentication.
-    [parameter(ParameterSetName = "SQLAuthentication", Mandatory = $false)]
+    # Specifies to use SQL Authentication when connecting to the MSSQL Server.
+    [parameter(ParameterSetName = "SQLAuthentication", Mandatory = $true)]
     [switch]
     $SQLAuthentication,
 
-    # The username to authenticate with.
-    # This is required when using SQL authentication, but has no effect when using Windows authentication.
+    # Specifies the username to use when authenticating to the MSSQL Server.
+    # This parameter is only used when authenticating with SQL Authentication.
     [parameter(ParameterSetName = "SQLAuthentication", Mandatory = $true)]
     [String]
     $Username
